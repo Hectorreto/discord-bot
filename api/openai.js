83350e87819083a5b4dd2232497f2affed52a57e
Héctor de Anda <hdeandaf@hotmail.com>
2023-05-21 19:37:24 -0600

    Add context to completions

--- api/openai.js
+++ api/openai.js
@@ -9,12 +9,14 @@ export const createCompletion = async (message) => {
   try {
     const completion = await openai.createCompletion({
       model: 'text-davinci-003',
-      prompt: message,
+      prompt:
+        `Mensaje: ${message}\n` +
+        'Respuesta: ',
       temperature: 0.6,
       max_tokens: 2000
     })
     return completion.data.choices[0].text
   } catch (error) {
-    return 'Chatgpt quiere que pague :c'
+    return JSON.stringify(error.response?.data, null, 2)
   }
 }
