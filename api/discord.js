ee87ba5709edfdc0940f297dd77b3fbc407cfa78
Héctor de Anda <hdeandaf@hotmail.com>
2023-05-25 17:17:25 -0600

    separate login to update commands

--- api/discord.js
+++ api/discord.js
@@ -14,36 +14,40 @@ export const discordValidation = (req, res, buf) => {
   }
 }
 
+const updateCommands = (guildId, commands) => {
+  const applicationId = process.env.APP_ID
+  const API = 'https://discord.com/api/v10'
+  const endpoint = `/applications/${applicationId}/guilds/${guildId}/commands`
+
+  fetch(API + endpoint, {
+    method: 'PUT',
+    headers: {
+      Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(commands.map((command) => command.config))
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (!Array.isArray(data)) {
+        throw data
+      }
+      return data
+    })
+    .then((data) => data.map((command) => command.name))
+    .then((commands) => console.log({ guildId, commands }))
+    .catch((err) => console.log('Error cargando', guildId, JSON.stringify(err, null, 2)))
+}
+
 export const registerCommands = () => {
   const commands = [
     generar,
     generarImagen
   ]
-  const applicationId = process.env.APP_ID
+
   const guildIds = process.env.GUILD_IDS.split(' ')
-
   guildIds.forEach((guildId) => {
-    const API = 'https://discord.com/api/v10'
-    const endpoint = `/applications/${applicationId}/guilds/${guildId}/commands`
-
-    fetch(API + endpoint, {
-      method: 'PUT',
-      headers: {
-        Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(commands.map((command) => command.config))
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (!Array.isArray(data)) {
-          throw data
-        }
-        return data
-      })
-      .then((data) => data.map((command) => command.name))
-      .then((commands) => console.log({ guildId, commands }))
-      .catch((err) => console.log('Error cargando', guildId, JSON.stringify(err, null, 2)))
+    updateCommands(guildId, commands)
   })
 
   return Object.fromEntries(commands.map((value) =>
